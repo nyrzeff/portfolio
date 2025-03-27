c55d94b61af312a06733bc6d663788fc2535ad2d
nyrzeff <nyrghzef@keemail.me>
2025-03-27 20:40:22 +0000

    chore: update packages and minor config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  parser: "@typescript-eslint/parser",
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:prettier/recommended',
+    "react-app",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
   ],
-  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  plugins: ["@typescript-eslint", "react", "prettier"],
   rules: {
-    'react/prop-types': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'prettier/prettier': 'error',
+    "react/prop-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "prettier/prettier": "error",
   },
 };
-
