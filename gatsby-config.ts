08632feea9492d94ef043b26dc8378febd4b6aae
nyrzeff <nyrghzef@keemail.me>
2025-04-10 14:43:45 +0100

    chore(config): add path alias to src

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,9 +5,6 @@ const config: GatsbyConfig = {
     title: `Portfolio`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
     "gatsby-plugin-sass",
@@ -44,6 +41,15 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {},
+    },
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias: {
+          "@": "src"
+        },
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+      },
     },
   ],
 };
