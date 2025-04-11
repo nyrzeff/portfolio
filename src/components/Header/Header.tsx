6074ccb83bee43b8713bb9029245ce19753b7238
nyrzeff <nyrghzef@keemail.me>
2025-04-11 13:37:09 +0100

    feat: scaffold Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,33 @@
 import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import * as styles from "./Header.module.scss";
 
 export const Header: React.FC = () => {
-  return <div></div>;
+  const imagePath = "../../../favicon.png";
+
+  return (
+    <header className={styles.header}>
+      <a href="#">
+        <StaticImage
+          src={imagePath}
+          alt="Eye of Nyrzeff"
+          className={styles.heroImage}
+        />
+      </a>
+      <nav>
+        <input type="checkbox" id="checkbox" className={styles.checkbox} />
+        <label htmlFor="checkbox" className={styles.borgar}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+        <div className={styles.navMobile}>
+          <a href="#">Blog</a>
+          <a href="#">Stack</a>
+          <a href="#">Projects</a>
+          <a href="#">Contact me!</a>
+        </div>
+      </nav>
+    </header>
+  );
 };
