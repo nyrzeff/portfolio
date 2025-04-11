280a0a25b783d03d5baa441c339179180989fb11
nyrzeff <nyrghzef@keemail.me>
2025-04-11 20:28:05 +0100

    chore: add new vite configs and dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,37 @@
 import js from "@eslint/js";
-import eslintPluginImportX from "eslint-plugin-import-x";
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 import tsParser from "@typescript-eslint/parser";
+import eslintPluginImportX from "eslint-plugin-import-x";
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 
-export default [
-  js.configs.recommended,
-  eslintPluginImportX.flatConfigs.recommended,
-  eslintPluginImportX.flatConfigs.typescript,
+export default tseslint.config(
+  { ignores: ['dist'] },
+  extends: [
+    js.configs.recommended, 
+    ...tseslint.configs.recommended, 
+    eslintPluginImportX.flatConfigs.recommended,
+    eslintPluginImportX.flatConfigs.typescript,
+  ],
   {
     files: ["src/**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
     languageOptions: {
+      globals: globals.browser,
       parser: tsParser,
       ecmaVersion: "latest",
       sourceType: "module",
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
       "no-unused-vars": "off",
       "import-x/no-dynamic-require": "warn",
       "import-x/no-nodejs-modules": "warn",
@@ -26,4 +44,4 @@ export default [
       "import-x/no-default-export": "error",
     },
   },
-];
+);
