4dae9e4e88ac804daa50d84437443c12f1295aeb
nyrzeff <nyrghzef@keemail.me>
2025-04-14 21:02:31 +0100

    chore(styles): isolate SideMenu from rest of the content

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode, useState } from "react";
+import { createPortal } from "react-dom";
 import { Header, SideMenu, Footer } from "@components";
 import styles from "./Layout.module.scss";
 
@@ -10,13 +11,20 @@ export const Layout = ({ children }: LayoutProps) => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div
-      className={`${styles["layout-wrapper"]} ${menuOpen ? styles["menu-open"] : ""}`}
-    >
-      <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <SideMenu isOpen={menuOpen} setIsOpen={setMenuOpen} />
-      <main className={styles["content"]}>{children}</main>
-      <Footer />
-    </div>
+    <>
+      <div
+        className={`${styles["layout-wrapper"]} ${menuOpen ? styles["menu-open"] : ""}`}
+      >
+        <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <main className={styles["content"]}>{children}</main>
+        <Footer />
+      </div>
+
+      {menuOpen &&
+        createPortal(
+          <SideMenu isOpen={menuOpen} setIsOpen={setMenuOpen} />,
+          document.getElementById("menu-root"),
+        )}
+    </>
   );
 };
