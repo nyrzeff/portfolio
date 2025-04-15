74b1308e1568af14f355106f55423ab4b15b3132
nyrzeff <nyrghzef@keemail.me>
2025-04-15 10:20:16 +0100

    style: enlarge SideMenu navigation link tap/click area and add scale effect on hover

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -21,30 +21,20 @@ export const SideMenu = ({ isOpen, setIsOpen }: SideMenuProps) => {
         <span></span>
       </button>
       <ul>
-        <li>
-          <a href="#intro" onClick={closeMenu}>
-            Intro
-          </a>
+        <li onClick={closeMenu}>
+          <a href="#intro">Intro</a>
         </li>
-        <li>
-          <a href="#blog" onClick={closeMenu}>
-            Blog
-          </a>
+        <li onClick={closeMenu}>
+          <a href="#blog">Blog</a>
         </li>
-        <li>
-          <a href="#stack" onClick={closeMenu}>
-            Stack
-          </a>
+        <li onClick={closeMenu}>
+          <a href="#stack">Stack</a>
         </li>
-        <li>
-          <a href="#projects" onClick={closeMenu}>
-            Projects
-          </a>
+        <li onClick={closeMenu}>
+          <a href="#projects">Projects</a>
         </li>
-        <li>
-          <a href="#contact" onClick={closeMenu}>
-            Contact me!
-          </a>
+        <li onClick={closeMenu}>
+          <a href="#contact">Contact me!</a>
         </li>
       </ul>
     </aside>
