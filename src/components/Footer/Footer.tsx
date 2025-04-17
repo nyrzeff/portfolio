196aa70920ff34a7933f4512c44babf979464b46
nyrzeff <nyrghzef@keemail.me>
2025-04-17 19:55:33 +0100

    feat: improve Footer styling [WIP]

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -21,58 +21,59 @@ export const Footer: React.FC<FooterProps> = ({ imagePath }): FooterProps => {
   return (
     <footer className={styles["footer"]}>
       <div className={styles["footer-line"]}></div>
-      <div className={styles["footer-top"]}>
-        <figure>
-          <img src={imagePath} alt="Eye of Nyrzeff" />
-          <div className={styles["caption-container"]}>
-            <figcaption>
-              Real Name: Felipe Ferreira{"\n"}Pseudonym: <b>Nyrzeff</b>
-            </figcaption>
-            <span>
-              <em>
-                Guided by <strong>The Elemental Septet</strong>
-              </em>
-            </span>
+      <div className={styles["footer-main"]}>
+        <div className={styles["footer-top"]}>
+          <figure>
+            <img src={imagePath} alt="Eye of Nyrzeff" />
+            <div className={styles["caption-container"]}>
+              <figcaption>
+                Pseudonym: <b>Nyrzeff</b>
+                {"\n"}
+                <em>
+                  Guided by <strong>The Elemental Septet</strong>
+                </em>
+              </figcaption>
+            </div>
+          </figure>
+        </div>
+        <nav className={styles["footer-middle"]}>
+          <div className={styles["nav-container"]}>
+            <h2>Navigation</h2>
+            <a href="#home">Home</a>
+            <a href="#intro">Intro</a>
+            <a href="#blog">Blog</a>
+            <a href="#stack">Stack</a>
+            <a href="#projects">Projects</a>
           </div>
-        </figure>
+          <div className={styles["social-container"]}>
+            <h2>Social</h2>
+            <a href="#contact" className={styles["icon-container"]}>
+              <Contact />
+              <span>Contact me</span>
+            </a>
+            <a
+              href="mailto:nyrghzef@keemail.me"
+              className={styles["icon-container"]}
+            >
+              <Mail />
+              <span>Mail me</span>
+            </a>
+            <a
+              href="https://github.com/nyrzeff"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles["icon-container"]}
+            >
+              <Github />
+              <span>GitHub</span>
+            </a>
+            <div className={styles["icon-container"]}>
+              <MapPin />
+              <span>Remote • {getUtcOffset()}</span>
+            </div>
+          </div>
+        </nav>
       </div>
-      <nav className={styles["footer-main"]}>
-        <div className={styles["nav-container"]}>
-          <h2>Navigation</h2>
-          <a href="#home">Home</a>
-          <a href="#intro">Intro</a>
-          <a href="#blog">Blog</a>
-          <a href="#stack">Stack</a>
-          <a href="#projects">Projects</a>
-        </div>
-        <div className={styles["social-container"]}>
-          <h2>Social</h2>
-          <a href="#contact" className={styles["icon-container"]}>
-            <Contact />
-            <span>Contact me</span>
-          </a>
-          <a
-            href="mailto:nyrghzef@keemail.me"
-            className={styles["icon-container"]}
-          >
-            <Mail />
-            <span>Mail me</span>
-          </a>
-          <a
-            href="https://github.com/nyrzeff"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles["icon-container"]}
-          >
-            <Github />
-            <span>GitHub</span>
-          </a>
-          <div className={styles["icon-container"]}>
-            <MapPin />
-            <span>Remote • {getUtcOffset()}</span>
-          </div>
-        </div>
-      </nav>
       <div className={styles["footer-line"]}></div>
       <div className={styles["footer-bottom"]}>
         <span>
