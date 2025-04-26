8fdbebd808677315afa6b60160e6b34347a697d0
nyrzeff <nyrghzef@keemail.me>
2025-04-26 19:43:35 +0100

    feat: improve Stack styles (add media query)

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -5,7 +5,7 @@ import styles from "./Intro.module.scss";
 
 export const Intro: React.FC = () => {
   return (
-    <section id={styles["intro"]}>
+    <section tabIndex={0} id={styles["intro"]}>
       <div
         className={styles["card"]}
         onClick={(e) =>
