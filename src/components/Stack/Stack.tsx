489e35c2a8205bdd5c72f8d71b8f081a2ba923ae
nyrzeff <nyrghzef@keemail.me>
2025-04-26 16:33:12 +0100

    feat: add stack items to stack section (devicon + item name)

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -1,5 +1,19 @@
-import * as React from "react";
+import { devicons } from "@assets/icons";
+import styles from "./Stack.module.scss";
 
 export const Stack: React.FC = () => {
-  return <div></div>;
+  return (
+    <section className={styles["stack-section"]}>
+      <h2>Stack (languages, libraries, frameworks, tools)</h2>
+      <p>These are some of the technologies I've worked with, hover over them to see my current level of experience on each of them:</p>
+      <ul className={styles["stack-list"]}>
+        {Object.entries(devicons).map((item, index) => (
+          <li key={index}>
+            <i className={item[1]}></i>
+            <span>{item[0]}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
