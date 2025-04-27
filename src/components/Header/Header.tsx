37ee039bcbb3d4b3f41eb4e3296bab4efda4c004
nyrzeff <nyrghzef@keemail.me>
2025-04-28 00:00:57 +0100

    feat: reimplement separate Headers
    - Added HeaderMobile (previously SmallHeader) and HeaderDesktop
      (previously BigHeader) because the layout change is too significant to
    merely change styling with CSS and the side menu is incompatible with
    the centered content in Desktop mode

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
-import { icons } from "@assets/icons";
-import styles from "./Header.module.scss";
+import { useScreen } from "@/context/ScreenContext";
+import { HeaderMobile } from "./HeaderMobile";
+import { HeaderDesktop } from "./HeaderDesktop";
 
 interface HeaderProps {
   isOpen: boolean;
@@ -10,26 +11,15 @@ export const Header: React.FC<HeaderProps> = ({
   isOpen,
   setIsOpen,
 }: HeaderProps) => {
-  const Eye = icons["eye"];
+  const { isDesktopExperience } = useScreen();
 
   return (
-    <header className={styles["small-header"]}>
-      <a href="#" className={styles["hero-image-container"]}>
-        <Eye alt="Eye of Nyrzeff" className={styles["hero-image"]} />
-      </a>
-      <div className={styles["batatas-container"]}>
-        <button
-          className={`${styles["batatas"]} ${isOpen ? styles["open"] : ""}`}
-          onClick={() => setIsOpen(!isOpen)}
-          style={{
-            zIndex: 100,
-          }}
-        >
-          <span className={styles["batatinhas"]}></span>
-          <span className={styles["batatinhas"]}></span>
-          <span className={styles["batatinhas"]}></span>
-        </button>
-      </div>
-    </header>
+    <>
+      {isDesktopExperience ? (
+        <HeaderDesktop />
+      ): (
+        <HeaderMobile isOpen={isOpen} setIsOpen={setIsOpen} />
+      )}
+    </>
   );
 };
