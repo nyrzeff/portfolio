879add84dc9e97e495ce98a4e12763f6539f9817
nyrzeff <nyrghzef@keemail.me>
2025-04-30 17:12:16 +0100

    feat: implement Contact UI (needs adjustments for larger screens)

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,5 +1,22 @@
-import * as React from "react";
+import styles from "./Contact.module.scss";
 
 export const Contact: React.FC = () => {
-  return <div></div>;
+  return (
+    <section id={styles["contact"]}>
+      <h2>Contact me!</h2>
+      <p>No need for formalities, whether you want to hire me, discuss a project/idea or simply talk, just drop a message below:</p>
+      <form className={styles["form"]}>
+        <label className={styles["name"]}>Name
+          <input type="text" />
+        </label>
+        <label className={styles["email"]}>Email
+          <input type="text" />
+        </label>
+        <label className={styles["message"]}>Message
+          <textarea />
+        </label>
+        <input type="submit" value="Send" className={styles["send"]}/>
+      </form>
+    </section>
+  );
 };
