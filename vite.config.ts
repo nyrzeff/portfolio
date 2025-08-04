9cf8e85dd5f0a37cb9f2fe1dbf53790382714fee
nyrzeff <nyrghzef@keemail.me>
2025-08-04 18:55:29 +0100

    fix: add polyfill plugin in order to use Node functions in the browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [react(), tsconfigPaths(), svgr(), nodePolyfills()],
   assetsInclude: ["**/*.md"],
   build: {
     rollupOptions: {
       output: {
         assetFileNames: `assets/[name].[ext]`
       },
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis",
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
     },
   },
 });
