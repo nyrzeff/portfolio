b3527a0aff7e1e0cd59379881298d04b56ca5bdf
nyrzeff <nyrghzef@keemail.me>
2025-08-05 16:30:25 +0100

    feat: use smaller project cards on larger screens + visual improvements

--- src/components/projects/ImageGallery/ImageGallery.tsx
+++ src/components/projects/ImageGallery/ImageGallery.tsx
@@ -1,15 +1,19 @@
 import styles from "./ImageGallery.module.scss";
 
 interface ImageData {
+  title: string;
+  subtitle: string;
   images: string[];
 }
 
-export const ImageGallery: React.FC<ImageData> = ({ images }: ImageData) => {
+export const ImageGallery: React.FC<ImageData> = ({ title, subtitle, images }: ImageData) => {
+  let imageDescriptor = `${title} - ${subtitle}`;
+
   return (
     <div className={styles["image-container"]}>
       {images.map((image, index) => (
         <div key={index} className={styles["image"]}>
-          <img src={image ?? ""} alt={`Image ${index + 1}`} />
+          <img src={image ?? ""} alt={`${imageDescriptor} (image ${index + 1})`} />
         </div>
       ))}
     </div>
