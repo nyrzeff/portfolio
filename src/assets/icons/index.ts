63b6299232668d0b8f65d9bf6663e036161e65a3
nyrzeff <nyrghzef@keemail.me>
2025-11-12 23:41:01 +0000

    feat: significantly simplify the Stack section

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -13,344 +13,75 @@ export const icons = {
 
 export const stackItems: StackItem[] = [
   {
-    title: ".NET Core",
-    devicon: "devicon-dotnetcore-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Proficient,
-      isActivelyLearning: true,
-    },
-  },
-  {
-    title: "React",
-    devicon: "devicon-react-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Proficient,
-      isActivelyLearning: true,
-    },
-  },
-  {
     title: "C#",
     devicon: "devicon-csharp-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Proficient,
-      isActivelyLearning: true,
-    },
-  },
-  {
-    title: "JavaScript",
-    devicon: "devicon-javascript-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Proficient,
-    },
-  },
-  {
-    title: "TypeScript",
-    devicon: "devicon-typescript-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Proficient,
-    },
-  },
-  {
-    title: "Bash",
-    devicon: "devicon-bash-plain",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "C",
-    devicon: "devicon-c-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Familiar,
-    },
-  },
-  {
-    title: "Python",
-    devicon: "devicon-python-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Familiar,
-    },
-  },
-  {
-    title: "Rust",
-    devicon: "devicon-rust-plain",
-    additionalInfo: {
-      skillLevel: SkillLevel.Novice,
-    },
-  },
-  {
-    title: "HTML5",
-    devicon: "devicon-html5-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Proficient,
-    },
-  },
-  {
-    title: "CSS3",
-    devicon: "devicon-css3-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "Sass",
-    devicon: "devicon-sass-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "Vue.js",
-    devicon: "devicon-vuejs-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "Vuetify",
-    devicon: "devicon-vuetify-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "Gatsby",
-    devicon: "devicon-gatsby-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Familiar,
-    },
-  },
-  {
-    title: "Tailwind CSS",
-    devicon: "devicon-tailwindcss-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Novice,
-      isActivelyLearning: true,
-    },
-  },
-  {
-    title: "Node.js",
-    devicon: "devicon-nodejs-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "Mocha",
-    devicon: "devicon-mocha-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "MongoDB",
-    devicon: "devicon-mongodb-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Familiar,
-    },
-  },
-  {
-    title: "PostgreSQL",
-    devicon: "devicon-postgresql-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Novice,
-      isActivelyLearning: true,
-    },
-  },
-  {
-    title: "Microsoft SQL Server",
-    devicon: "devicon-microsoftsqlserver-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "Elasticsearch",
-    devicon: "devicon-elasticsearch-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Familiar,
-    },
-  },
-  {
-    title: "Express",
-    devicon: "devicon-express-original",
-    additionalInfo: {
-      skillLevel: SkillLevel.Novice,
-    },
-  },
-  {
-    title: "Git",
-    devicon: "devicon-git-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Proficient,
-    },
-  },
-  {
-    title: "GitHub",
-    devicon: "devicon-github-original",
-    additionalInfo: {
-      skillLevel: SkillLevel.Proficient,
-    },
-  },
-  {
-    title: "GitLab",
-    devicon: "devicon-gitlab-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Familiar,
-    },
-  },
-  {
-    title: "ESLint",
-    devicon: "devicon-eslint-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "Vite",
-    devicon: "devicon-vite-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Proficient,
-    },
-  },
-  {
-    title: "Webpack",
-    devicon: "devicon-webpack-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Familiar,
-    },
-  },
-  {
-    title: "Docker",
-    devicon: "devicon-docker-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Familiar,
-    },
-  },
-  {
-    title: "Jenkins",
-    devicon: "devicon-jenkins-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Familiar,
-    },
-  },
-  {
-    title: "Linux",
-    devicon: "devicon-linux-plain",
-    additionalInfo: {
-      skillLevel: SkillLevel.Proficient,
-    },
-  },
-  {
-    title: "Arch Linux",
-    devicon: "devicon-archlinux-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "Vim",
-    devicon: "devicon-vim-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "Neovim",
-    devicon: "devicon-neovim-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "PNPM",
-    devicon: "devicon-pnpm-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Proficient,
-    },
-  },
-  {
-    title: "Homebrew",
-    devicon: "devicon-homebrew-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "Next.js",
-    devicon: "devicon-nextjs-plain",
-    additionalInfo: {
-      skillLevel: SkillLevel.Novice,
-    },
   },
   {
     title: "Java",
     devicon: "devicon-java-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Familiar,
-    },
   },
   {
-    title: "Gradle",
-    devicon: "devicon-gradle-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Novice,
-    },
+    title: "HTML5",
+    devicon: "devicon-html5-plain colored",
+  },
+  {
+    title: "CSS3",
+    devicon: "devicon-css3-plain colored",
+  },
+  {
+    title: "JavaScript",
+    devicon: "devicon-javascript-plain colored",
+  },
+  {
+    title: "TypeScript",
+    devicon: "devicon-typescript-plain colored",
+  },
+  {
+    title: ".NET Core",
+    devicon: "devicon-dotnetcore-plain colored",
+  },
+  {
+    title: "React",
+    devicon: "devicon-react-plain colored",
+  },
+  {
+    title: "Node.js",
+    devicon: "devicon-nodejs-plain colored",
+  },
+  {
+    title: "PostgreSQL",
+    devicon: "devicon-postgresql-plain colored",
+  },
+  {
+    title: "Microsoft SQL Server",
+    devicon: "devicon-microsoftsqlserver-plain colored",
+  },
+  {
+    title: "Sass",
+    devicon: "devicon-sass-plain colored",
+  },
+  {
+    title: "Mocha",
+    devicon: "devicon-mocha-plain colored",
+  },
+  {
+    title: "Git",
+    devicon: "devicon-git-plain colored",
+  },
+  {
+    title: "Linux",
+    devicon: "devicon-linux-plain",
   },
   {
     title: "Figma",
     devicon: "devicon-figma-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Familiar,
-    },
-  },
-  {
-    title: "Inkscape",
-    devicon: "devicon-inkscape-plain",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
-  },
-  {
-    title: "GraphQL",
-    devicon: "devicon-graphql-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Familiar,
-    },
-  },
-  {
-    title: "JSON",
-    devicon: "devicon-json-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Proficient,
-    },
-  },
-  {
-    title: "Lua",
-    devicon: "devicon-lua-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Novice,
-    },
-  },
-  {
-    title: "Markdown",
-    devicon: "devicon-markdown-plain",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
   },
   {
     title: "OpenAPI",
     devicon: "devicon-openapi-plain colored",
-    additionalInfo: {
-      skillLevel: SkillLevel.Competent,
-    },
   },
   {
     title: "Vercel",
     devicon: "devicon-vercel-plain",
-    additionalInfo: {
-      skillLevel: SkillLevel.Familiar,
-    },
   },
 ];
--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -1,64 +1,20 @@
-import type { StackItem } from "@/types/stack";
 import { stackItems } from "@assets/icons";
 import styles from "./Stack.module.scss";
 
-interface TooltipProps {
-  item: StackItem;
+interface StackItem {
+  title: string;
+  devicon: string;
 }
-
-const Tooltip: React.FC<TooltipProps> = ({ item }: TooltipProps) => {
-  return (
-    <div className={styles["tooltip"]}>
-      <p className={styles["tooltip-text"]}>
-        <b>Skill level</b>: {item.additionalInfo.skillLevel}
-        <br />
-        {item.additionalInfo.isActivelyLearning && <em>Actively learning</em>}
-      </p>
-    </div>
-  );
-};
 
 export const Stack: React.FC = () => {
   return (
     <section tabIndex={0} id="stack" className={styles["stack"]}>
       <header className={styles["stack-header"]}>
         <h2>Stack</h2>
-        <p>
-          These are some of the technologies (languages, libraries, frameworks,
-          tools and other systems) I've worked with, either in personal,
-          academic or professional settings, with varying levels of competency.
-          Tap/hover over each item for additional information. For more
-          information about my development environment, take a look at the About
-          section.
-        </p>
-        <fieldset>
-          <legend>Skill levels</legend>
-          <span>
-            <strong>Novice:</strong> Beginner
-          </span>
-          <span>
-            <strong>Familiar:</strong> Advanced Beginner
-          </span>
-          <span>
-            <strong>Competent:</strong> Intermediate
-          </span>
-          <span>
-            <strong>Proficient:</strong> Advanced
-          </span>
-          <span>
-            <strong>Master:</strong> Expert
-          </span>
-          <p>
-            Actively learning means I'm currently upskilling in the highlighted
-            technology (building projects, updating mental models and reading
-            docs, references, books and articles on it){" "}
-          </p>
-        </fieldset>
       </header>
       <ul className={styles["stack-list"]}>
         {stackItems.map((item: StackItem, index) => (
           <li key={index} className={styles["stack-list-item"]}>
-            <Tooltip item={item} />
             <div className={styles["stack-item-container"]}>
               <i className={item.devicon}></i>
               <span>
