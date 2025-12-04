38efc78254499b1d19fa01c88f4e8fb9ddc09076
nyrzeff <nyrghzef@keemail.me>
2025-12-04 10:44:28 +0000

    feat: implement a crude Timeline

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { Layout } from "./layout";
 export { Intro } from "./Intro";
 export { Stack } from "./Stack";
+export { Timeline } from "./Timeline";
 export { Projects } from "./projects";
 export { Contact } from "./Contact";
