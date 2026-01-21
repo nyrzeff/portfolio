5fa8b0cbb53e70a6cc5ae240de864f8ba7b9ad13
nyrzeff <nyrghzef@keemail.me>
2026-01-21 16:46:32 +0000

    refactor: remove gray-matter, implement a custom Markdown frontmatter YAML to JSON parser, temporarily remove manualChunks because they're breaking production

--- src/lib/parseMarkdown.ts
+++ src/lib/parseMarkdown.ts
@@ -1,14 +1,45 @@
-import { type GrayMatterFile, default as matter } from "gray-matter";
 import type { Frontmatter, MarkdownFile } from "@/types/markdown";
 
-export const parseMarkdown = (raw: string): MarkdownFile => {
-    const { data, content } = matter(raw);
-    const frontmatter = data as Frontmatter;
+const parseFrontmatter = (raw: string): Frontmatter => {
+    type StringKey = "id" | "title" | "subtitle";
+    type ArrayKey = "tags" | "stack" | "colors";
 
-    return {
-        frontmatter,
-        content,
+    const frontmatter: Frontmatter = {
+        id: "",
+        title: "",
+        subtitle: "",
+        tags: [],
+        stack: [],
+        colors: [],
     };
+
+    const chunks = raw.split("\n");
+
+    let arrayPropertyKey;
+    let arrayPropertyValue = [];
+
+    for (const chunk of chunks) {
+        if (chunk.includes(":") && chunk.includes('"')) {
+            const fragments = chunk.split(":");
+            const propertyKey = fragments[0].trim() as StringKey;
+            const propertyValue = fragments[1].trim().slice(1, -1) as string;
+            frontmatter[propertyKey] = propertyValue;
+        } else {
+            if (chunk.charAt(chunk.length - 1) === ":") {
+                arrayPropertyValue = [];
+                arrayPropertyKey = chunk.slice(0, -1) as ArrayKey;
+                frontmatter[arrayPropertyKey] = [];
+            }
+
+            if (chunk.includes("-") && arrayPropertyKey !== undefined) {
+                let value = chunk.trim().substring(2);
+                if (value.includes('"')) value = value.slice(1, -1);
+                arrayPropertyValue.push(value);
+                frontmatter[arrayPropertyKey] = arrayPropertyValue;
+            }
+        }
+    }
+    return frontmatter;
 };
 
 // all .md files are inside the same directory anyway,
@@ -21,9 +52,12 @@ export const parseMarkdownFromDir = async (): Promise<MarkdownFile[]> => {
 
     const projects = await Promise.all(
         Object.entries(modules).map(async ([, loader]) => {
-            const raw = (await loader()) as GrayMatterFile<string>;
-            const { data, content } = matter(raw);
-            const frontmatter = data as Frontmatter;
+            const raw = (await loader()) as string;
+
+            const chunks = raw.split("---").slice(1);
+            const rawFrontmatter: string = chunks[0].trim();
+            const frontmatter: Frontmatter = parseFrontmatter(rawFrontmatter);
+            const content: string = chunks[1].trim();
 
             return {
                 frontmatter,
